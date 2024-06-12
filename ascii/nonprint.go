2f30528a00f34c1994038f8d381f083fee585858
doonyango <datieno001@gmail.com>
2024-06-12 12:28:34 +0300

    docs: document function

--- ascii/nonprint.go
+++ ascii/nonprint.go
@@ -1,5 +1,7 @@
 package ascii
 
+// Checks for non-printable ascii-art characters
+// and exludes them from the program.
 func ContainNonPrintChar(words string) bool {
 	for _, char := range words {
 		if char < 32 || char > 126 {
